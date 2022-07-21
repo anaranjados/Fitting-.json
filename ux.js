abb9d7e4b27335fb212852f4933d1619d60538c2
Alejandro Proenza <dev@laraceramica.com>
2022-07-21 09:21:15 +0300

    improved lookup

--- ux.js
+++ ux.js
@@ -170,19 +170,18 @@ const
    gm_lft_img = qs('#menu_general img[alt=left]'),
    
    sh		   = qs('#shadow'),
-   tab_body = qs('tbody'),
-   tab_col_n= qs('header .columns').childElementCount, // qs('thead tr').childElementCount,
+   tab_body = qs('main'),
+   tab_col_n= qs('header .columns').childElementCount,
    sch 	   = qs('#search'),
    enty_dfl = 7,
    enty_step= 2,
    max_H 	= document.body.scrollHeight || document.body.offsetHeight || document.clientHeight || document.scrollHeight || document.offsetHeight,
    ava		= '<svg width="25" height="25"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm8.127 19.41c-.282-.401-.772-.654-1.624-.85-3.848-.906-4.097-1.501-4.352-2.059-.259-.565-.19-1.23.205-1.977 1.726-3.257 2.09-6.024 1.027-7.79-.674-1.119-1.875-1.734-3.383-1.734-1.521 0-2.732.626-3.409 1.763-1.066 1.789-.693 4.544 1.049 7.757.402.742.476 1.406.22 1.974-.265.586-.611 1.19-4.365 2.066-.852.196-1.342.449-1.623.848 2.012 2.207 4.91 3.592 8.128 3.592s6.115-1.385 8.127-3.59zm.65-.782c1.395-1.844 2.223-4.14 2.223-6.628 0-6.071-4.929-11-11-11s-11 4.929-11 11c0 2.487.827 4.783 2.222 6.626.409-.452 1.049-.81 2.049-1.041 2.025-.462 3.376-.836 3.678-1.502.122-.272.061-.628-.188-1.087-1.917-3.535-2.282-6.641-1.03-8.745.853-1.431 2.408-2.251 4.269-2.251 1.845 0 3.391.808 4.24 2.218 1.251 2.079.896 5.195-1 8.774-.245.463-.304.821-.179 1.094.305.668 1.644 1.038 3.667 1.499 1 .23 1.64.59 2.049 1.043z"/></svg>',
-   // wait_icn = '<svg width="100" id="waiter"><path d="M3.968,12.061C1.775,12.061,0,13.835,0,16.027c0,2.192,1.773,3.967,3.968,3.967c2.189,0,3.966-1.772,3.966-3.967    C7.934,13.835,6.157,12.061,3.968,12.061z M16.233,12.061c-2.188,0-3.968,1.773-3.968,3.965c0,2.192,1.778,3.967,3.968,3.967         s3.97-1.772,3.97-3.967C20.201,13.835,18.423,12.061,16.233,12.061z M28.09,12.061c-2.192,0-3.969,1.774-3.969,3.967 c0,2.19,1.774,3.965,3.969,3.965c2.188,0,3.965-1.772,3.965-3.965S30.278,12.061,28.09,12.061z"/> </g> </svg>',
    edit_btn = '<button>Правка</button>'
    
 
 function init(enty) {
-   if( typeof(enty) === 'number' )
+   if( typeof(enty) === 'number' )        // mayor way
       n = c
    else if( typeof(enty) === 'object' )	// search way
       n = enty
@@ -215,7 +214,7 @@ function init(enty) {
    tab_body.innerHTML = ''
    n.forEach(rec => {
       const t = {
-         row: document.createElement('tr'),
+         row: document.createElement('section'),
          data: new Array
       }
       function fill_row() {
@@ -223,7 +222,7 @@ function init(enty) {
             t.data[i].innerHTML = arguments[i]
       }
       for (let j=0; j<tab_col_n; j++)
-         t.data[j] = document.createElement('td')
+         t.data[j] = document.createElement('div')
 
       for (let j=0; j<tab_col_n; j++) {
          fill_row(
@@ -237,12 +236,13 @@ function init(enty) {
             edit_btn
          )
          t.row.appendChild( t.data[j] )
+         t.row.className = 'columns'
       }
       tab_body.appendChild( t.row )   
    })
 
-   const rows = qsa('tbody tr'),    // Action edit btn for each row
-         btn = qsa('tbody button')
+   const rows = qsa('main section'),    // Action edit btn for each row
+         btn = qsa('main button')
    for( let i=0; i<rows.length; i++ ) {
       btn[i].onclick= (ev)=>	{
          show(cf, sh)
@@ -266,12 +266,12 @@ function init(enty) {
 // Contact form
 function save_c(flag) {
    cf_save.onclick= ()=> {
-      if (cf_inp[0].value.length > 0 || cf_inp[1].value.length > 0) {
+      if (cf_inp[0].value.length > 0 || cf_inp[1].value.length > 0) {   // check if Name or Surname is set (in form)
          if (flag === 'insert') {
             let next_i = 0;
             for (var i in c) {         
                if ( c[i].id >= next_i )
-                     next_i = c[i].id +1   // set next id value for Object
+                  next_i = c[i].id +1   // set next id value for Object
             }
             apply_c(next_i)
             entities += 1
@@ -359,8 +359,5 @@ sch.onkeypress= ()=> {
                )
             return e
    })
-   
-   console.log( found )
-   
    init(found)
 }
